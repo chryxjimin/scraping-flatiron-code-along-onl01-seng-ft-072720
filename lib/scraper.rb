a6c6098a04ed4a61bc6e9b78dc42b4cfc433d5cd
Christie Yoon <cleex1999@gmail.com>
2020-08-26 21:49:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,4 +20,13 @@ class Scraper
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
+
+    def get_courses
+
+    end
+
+  def make_courses
+
+  end
+
 end
